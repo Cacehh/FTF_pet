e4bd21b377280d08790ef5a6ceac3502b090f64a
DyRolfCayce <caycedy@gmail.com>
2016-12-11 14:22:47 +0800

    Post deleted some unnecessary files

---
+++ application/views/snips/a_restrict.php
@@ -0,0 +1,10 @@
+
+<!-- PHP function to check sessions to pages that would include the code below -->
+<?php
+if (isset($this->session->userdata['logged_in'])) {
+	$username = ($this->session->userdata['logged_in']['username']);
+	// $email = ($this->session->userdata['logged_in']['email']);
+} else {
+	header("location: login");
+}
+?>
