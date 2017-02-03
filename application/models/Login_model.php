36f6be3a1ddb7b98a5ffe53f7c940e9f2ef37639
Rolf Cayce Dy <caycedy@gmail.com>
2017-02-03 11:12:27 +0800

    Added the login Model

---
+++ application/models/Login_model.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * @name: Login model
+ * @author: Imron Rosdiana
+ */
+class Login_model extends CI_Model
+{
+
+    function __construct() {
+        parent::__construct();
+        $this->load->database();
+    }
+
+    public function validate_user($data) {
+        $this->db->where('username', $data['username']);
+        $this->db->where('user_password', md5($data['user_password']));
+        // $this->db->where('acct_type', $data['acct_type']);
+        // $this->db->where('user_email', $data['user_email']);
+        return $this->db->get('users')->row();
+
+        // $user_array = array('username' => $username, 'user_password' => $user_password, 'acct_type' => $acct_type);
+        // $this->db->where($user_array);
+    }    
+
+    // public function detect_user($data) {
+    //     $this->db->select('acct_type',$data['acct_type']);
+    // }
+
+    function __destruct() {
+        $this->db->close();
+    }
+}
