eb12b59542a0ad1015dff6bd8f54a4f073c2811d
Rolf Cayce Dy <caycedy@gmail.com>
2017-02-03 11:05:25 +0800

    Updated the user table

--- database/users.sql
+++ database/users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 11, 2016 at 07:35 AM
+-- Generation Time: Feb 03, 2017 at 03:04 AM
 -- Server version: 5.7.14
 -- PHP Version: 5.6.25
 
@@ -28,17 +28,22 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `user_name` varchar(255) NOT NULL,
-  `user_email` varchar(255) NOT NULL,
-  `user_password` varchar(255) NOT NULL
+  `FirstName` varchar(40) DEFAULT NULL,
+  `LastName` varchar(40) DEFAULT NULL,
+  `username` varchar(40) NOT NULL,
+  `user_email` varchar(40) DEFAULT NULL,
+  `user_password` varchar(40) NOT NULL,
+  `acct_type` int(10) NOT NULL DEFAULT '0' COMMENT '0B,1IM,2POS,3SA'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `user_name`, `user_email`, `user_password`) VALUES
-(1, 'Pedro', 'pedro@gmail.com', '123');
+INSERT INTO `users` (`id`, `FirstName`, `LastName`, `username`, `user_email`, `user_password`, `acct_type`) VALUES
+(4, 'user', 'user', 'user', 'user', 'ee11cbb19052e40b07aac0ca060c23ee', 3),
+(33, NULL, NULL, 'user1', NULL, '24c9e15e52afc47c225b757e7bee1f9d', 0),
+(34, NULL, NULL, 'user2', NULL, '7e58d63b60197ceb55a1c487989a3720', 0);
 
 --
 -- Indexes for dumped tables
@@ -58,7 +63,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
