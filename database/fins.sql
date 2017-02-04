6cc43b0c93a8b11e24f2b0601733ea7c1e38b51e
EAS\Alyson <belinoalysson@gmail.com>
2017-02-04 16:45:04 +0800

    Updated fins schema

--- database/fins.sql
+++ database/fins.sql
@@ -1,136 +1,265 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+CREATE DATABASE  IF NOT EXISTS `fins` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `fins`;
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 03, 2017 at 12:35 AM
--- Server version: 5.7.9
--- PHP Version: 5.6.15
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: fins
+-- ------------------------------------------------------
+-- Server version	5.7.14
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `fins`
---
-DROP DATABASE `fins`;
-CREATE DATABASE IF NOT EXISTS `fins` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `fins`;
-
--- --------------------------------------------------------
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `breeder`
 --
 
 DROP TABLE IF EXISTS `breeder`;
-CREATE TABLE IF NOT EXISTS `breeder` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `breeder` (
   `breederID` int(11) NOT NULL,
   `breederName` varchar(45) NOT NULL,
   `breederAddress` varchar(45) NOT NULL,
   `contactNumber` int(11) NOT NULL,
-  KEY `breederID` (`breederID`)
+  `prodID` int(11) NOT NULL,
+  KEY `breederID` (`breederID`),
+  KEY `prodID_idx` (`prodID`),
+  CONSTRAINT `prodID` FOREIGN KEY (`breederID`) REFERENCES `breeder` (`prodID`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `breeder`
+--
+
+LOCK TABLES `breeder` WRITE;
+/*!40000 ALTER TABLE `breeder` DISABLE KEYS */;
+/*!40000 ALTER TABLE `breeder` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `customers`
 --
 
 DROP TABLE IF EXISTS `customers`;
-CREATE TABLE IF NOT EXISTS `customers` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customers` (
   `customerID` int(11) NOT NULL AUTO_INCREMENT,
   `customerName` varchar(100) NOT NULL,
   `customerAddress` varchar(100) NOT NULL,
   PRIMARY KEY (`customerID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `customers`
 --
 
-INSERT INTO `customers` (`customerID`, `customerName`, `customerAddress`) VALUES
-(1, 'Hugh Jackman', 'New York City'),
-(2, 'Magnetto', 'Sultan Kudarat'),
-(3, 'Wolverine', 'Saint Louis University');
-
--- --------------------------------------------------------
+LOCK TABLES `customers` WRITE;
+/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+INSERT INTO `customers` VALUES (1,'Hugh Jackman','New York City'),(2,'Magnetto','Sultan Kudarat'),(3,'Wolverine','Saint Louis University');
+/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `order`
 --
 
 DROP TABLE IF EXISTS `order`;
-CREATE TABLE IF NOT EXISTS `order` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `order` (
   `orderID` int(11) NOT NULL,
   `orderDate` date NOT NULL,
   `orderQuantity` int(11) NOT NULL,
-  PRIMARY KEY (`orderID`),
-  KEY `customerID` (`orderID`),
-  KEY `productID` (`orderID`)
+  `customerID` int(11) NOT NULL,
+  PRIMARY KEY (`orderID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `order`
 --
 
-INSERT INTO `order` (`orderID`, `orderDate`, `orderQuantity`) VALUES
-(1, '2016-12-11', 2);
+LOCK TABLES `order` WRITE;
+/*!40000 ALTER TABLE `order` DISABLE KEYS */;
+INSERT INTO `order` VALUES (1,'2016-12-11',2,0);
+/*!40000 ALTER TABLE `order` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- --------------------------------------------------------
+--
+-- Table structure for table `ordpr`
+--
+
+DROP TABLE IF EXISTS `ordpr`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ordpr` (
+  `prodID` int(11) NOT NULL,
+  `orderID` int(11) NOT NULL,
+  PRIMARY KEY (`prodID`),
+  KEY `prodID_idx` (`prodID`,`orderID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ordpr`
+--
+
+LOCK TABLES `ordpr` WRITE;
+/*!40000 ALTER TABLE `ordpr` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ordpr` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `payment`
 --
 
 DROP TABLE IF EXISTS `payment`;
-CREATE TABLE IF NOT EXISTS `payment` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `payment` (
   `paymentID` int(11) NOT NULL AUTO_INCREMENT,
   `paymentDate` date NOT NULL,
   `paymentTime` varchar(45) NOT NULL,
   `paymentAmount` int(11) NOT NULL,
   `paymentType` varchar(45) NOT NULL,
-  PRIMARY KEY (`paymentID`,`paymentType`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  KEY `customerID` (`paymentID`,`paymentType`,`paymentAmount`,`paymentTime`,`paymentDate`),
+  CONSTRAINT `customerID` FOREIGN KEY (`paymentID`) REFERENCES `customers` (`customerID`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `payment`
+--
+
+LOCK TABLES `payment` WRITE;
+/*!40000 ALTER TABLE `payment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `payment` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `product`
 --
 
 DROP TABLE IF EXISTS `product`;
-CREATE TABLE IF NOT EXISTS `product` (
-  `productID` int(11) NOT NULL,
-  `productAmount` int(11) NOT NULL,
-  `productQuantity` int(11) NOT NULL,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product` (
+  `prodID` int(11) NOT NULL AUTO_INCREMENT,
+  `prodName` varchar(45) NOT NULL,
+  `prodAmount` int(11) NOT NULL,
+  `inStock` int(11) NOT NULL,
   `category` varchar(45) NOT NULL,
   `datePurchased` date NOT NULL,
-  PRIMARY KEY (`productID`,`datePurchased`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`prodID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (1,'pedigree',100,2,'dog food','2016-12-31'),(2,'12X12X8',450,1,'aquarium','2017-01-12'),(3,'meow purr',100,5,'cat food','2017-03-12'),(4,'birdie',135,4,'bird feeds','2017-04-15'),(5,'jumpy rabbit',200,3,'rabbit','2017-03-17');
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sales`
+--
+
+DROP TABLE IF EXISTS `sales`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sales` (
+  `salesID` int(11) NOT NULL,
+  `salesDate` date NOT NULL,
+  `salesQuantity` varchar(45) NOT NULL,
+  `receiptNumber` int(11) NOT NULL,
+  `prodID` int(11) NOT NULL,
+  PRIMARY KEY (`salesID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sales`
+--
+
+LOCK TABLES `sales` WRITE;
+/*!40000 ALTER TABLE `sales` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sales` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sprod`
+--
+
+DROP TABLE IF EXISTS `sprod`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sprod` (
+  `salesID` int(11) NOT NULL,
+  `prodID` int(11) NOT NULL,
+  PRIMARY KEY (`salesID`,`prodID`),
+  KEY `salesID_idx` (`salesID`),
+  KEY `prodID_idx` (`prodID`),
+  CONSTRAINT `salesID` FOREIGN KEY (`salesID`) REFERENCES `sales` (`salesID`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sprod`
+--
+
+LOCK TABLES `sprod` WRITE;
+/*!40000 ALTER TABLE `sprod` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sprod` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `supplier`
 --
 
 DROP TABLE IF EXISTS `supplier`;
-CREATE TABLE IF NOT EXISTS `supplier` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `supplier` (
   `supplierID` int(11) NOT NULL,
   `supplierName` varchar(45) NOT NULL,
   `supplierAddress` varchar(45) NOT NULL,
   `contactNumber` int(11) NOT NULL,
+  `prodID` int(11) NOT NULL,
   PRIMARY KEY (`supplierID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `supplier`
+--
+
+LOCK TABLES `supplier` WRITE;
+/*!40000 ALTER TABLE `supplier` DISABLE KEYS */;
+/*!40000 ALTER TABLE `supplier` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-02-04 16:42:45
