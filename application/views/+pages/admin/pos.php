713ff8648906f901bfca4d5d47aca379de987c50
Enzo Ronquillo <lorenzonoelronquillo@gmail.com>
2017-02-05 12:46:38 +0800

    created an inital test script for the computation of the total amount plus the discount

--- application/views/+pages/admin/pos.php
+++ application/views/+pages/admin/pos.php
@@ -1,3 +1,19 @@
+<!-- initial test script for computation of total amount plus discount. -->
+<script type="text/javascript">
+function calculate() {
+   var price=0, discount=0, afterDiscount=0;
+    price = Number(document.discountCalculator.price.value);
+    discount = Number(document.discountCalculator.discount.value);
+
+
+ afterDiscount=price - ( price*discount/100 );
+
+   document.discountCalculator.price.value=price.toFixed(2);
+   document.discountCalculator.discount.value=discount.toFixed(2);
+   document.discountCalculator.afterDiscount.value=afterDiscount.toFixed(2);
+
+}
+</script>
 
 <main>
 <br>
