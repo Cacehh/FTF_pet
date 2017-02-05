11fbf523ed496d2e87326caa1a73b6f7f459f30a
Rolf Cayce Dy <caycedy@gmail.com>
2017-02-05 23:13:11 +0800

    =====PLEASE IMPORT THE SQL FILE BEFORE TRYING===== Updated database for users to be used in the session handling

--- database/ftnf.sql
+++ database/ftnf.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 04, 2017 at 08:15 AM
+-- Generation Time: Feb 05, 2017 at 03:10 PM
 -- Server version: 5.7.14
 -- PHP Version: 5.6.25
 
@@ -36,6 +36,54 @@ CREATE TABLE `ci_sessions` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `ProdID` varchar(5) NOT NULL,
+  `ProdName` varchar(30) NOT NULL,
+  `Category` varchar(30) NOT NULL,
+  `Quantity` int(255) NOT NULL,
+  `Amount` decimal(10,0) NOT NULL,
+  `Timestamp` timestamp NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`ProdID`, `ProdName`, `Category`, `Quantity`, `Amount`, `Timestamp`) VALUES
+('00001', 'Product 1', 'Product', 100, '51', '2017-02-04 06:13:28'),
+('00002', 'Pet 1', 'Pet', 19, '981', '2017-02-04 06:13:28'),
+('00003', 'Product 2', 'Product', 50, '27', '2017-02-04 06:14:44'),
+('00004', 'Product 3', 'Product', 150, '70', '2017-02-04 06:14:44'),
+('00002', 'Pet 2', 'Pet', 91, '990', '2017-02-04 06:15:57'),
+('00003', 'Pet 3', 'Pet', 570, '550', '2017-02-04 06:15:57'),
+('00004', 'Product 4', 'Product', 50, '730', '2017-02-04 06:17:54'),
+('00005', 'Product 5', 'Product', 30, '181', '2017-02-04 06:17:54'),
+('00009', 'Sample', 'Product', 60, '100', '2017-02-04 07:30:27');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `Admin` varchar(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`Admin`) VALUES
+('POS'),
+('POS');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
@@ -60,13 +108,13 @@ INSERT INTO `user` (`id_user`, `username`, `password`) VALUES
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` int(150) NOT NULL,
   `FirstName` varchar(40) DEFAULT NULL,
   `LastName` varchar(40) DEFAULT NULL,
   `username` varchar(40) NOT NULL,
   `user_email` varchar(40) DEFAULT NULL,
   `user_password` varchar(40) NOT NULL,
-  `acct_type` int(10) NOT NULL DEFAULT '0' COMMENT '0B,1IM,2POS,3SA'
+  `acct_type` varchar(10) NOT NULL DEFAULT 'basic' COMMENT '0B,1IM,2POS,3SA'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -74,9 +122,13 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `FirstName`, `LastName`, `username`, `user_email`, `user_password`, `acct_type`) VALUES
-(4, 'user', 'user', 'user', 'user', 'ee11cbb19052e40b07aac0ca060c23ee', 3),
-(33, NULL, NULL, 'user1', NULL, '24c9e15e52afc47c225b757e7bee1f9d', 0),
-(34, NULL, NULL, 'user2', NULL, '7e58d63b60197ceb55a1c487989a3720', 0);
+(40, NULL, NULL, 'user3', NULL, '92877af70a45fd6a2ed7fe81e1236b78', 'SA'),
+(41, NULL, NULL, 'user0', NULL, '3d517fe6ebab7b8cfcf98db6259c8a59', 'Basic'),
+(39, NULL, NULL, 'user2', NULL, '7e58d63b60197ceb55a1c487989a3720', 'Inventory'),
+(38, NULL, NULL, 'user1', NULL, '24c9e15e52afc47c225b757e7bee1f9d', 'POS'),
+(101, 'Super', 'Admin', 'sa', 'sa@gmail.com', 'c12e01f2a13ff5587e1e9e4aedb8242d', 'SA'),
+(51, 'Inventory', 'Admin', 'ia', 'ia@gmail.com', 'b40788f566d1124a95169068077031a9', 'IA'),
+(1, 'POS', 'Admin', 'pa', 'pa@gmail.com', 'e529a9cea4a728eb9c5828b13b22844c', 'PA');
 
 --
 -- Indexes for dumped tables
@@ -109,11 +161,6 @@ ALTER TABLE `users`
 --
 ALTER TABLE `user`
   MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
