f94e48dee9dbd2b82a73d935f73c2a376d96270a
Rolf Cayce Dy <caycedy@gmail.com>
2017-02-07 09:56:18 +0800

    Updated the database for users and products

--- database/ftnf.sql
+++ database/ftnf.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 05, 2017 at 03:10 PM
+-- Generation Time: Feb 07, 2017 at 01:54 AM
 -- Server version: 5.7.14
 -- PHP Version: 5.6.25
 
@@ -53,53 +53,47 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`ProdID`, `ProdName`, `Category`, `Quantity`, `Amount`, `Timestamp`) VALUES
-('00001', 'Product 1', 'Product', 100, '51', '2017-02-04 06:13:28'),
-('00002', 'Pet 1', 'Pet', 19, '981', '2017-02-04 06:13:28'),
-('00003', 'Product 2', 'Product', 50, '27', '2017-02-04 06:14:44'),
-('00004', 'Product 3', 'Product', 150, '70', '2017-02-04 06:14:44'),
-('00002', 'Pet 2', 'Pet', 91, '990', '2017-02-04 06:15:57'),
-('00003', 'Pet 3', 'Pet', 570, '550', '2017-02-04 06:15:57'),
-('00004', 'Product 4', 'Product', 50, '730', '2017-02-04 06:17:54'),
-('00005', 'Product 5', 'Product', 30, '181', '2017-02-04 06:17:54'),
-('00009', 'Sample', 'Product', 60, '100', '2017-02-04 07:30:27');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `roles`
---
-
-CREATE TABLE `roles` (
-  `Admin` varchar(20) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `roles`
---
-
-INSERT INTO `roles` (`Admin`) VALUES
-('POS'),
-('POS');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE `user` (
-  `id_user` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`id_user`, `username`, `password`) VALUES
-(6, 'Imron', '5f4dcc3b5aa765d61d8327deb882cf99'),
-(7, 'user', 'ee11cbb19052e40b07aac0ca060c23ee');
+('00001', 'Affenpinscher', 'Pet Dog', 10, '6000', '2017-02-05 07:40:51'),
+('00002', 'Afghan Hound', 'Pet Dog', 10, '4580', '2017-02-05 07:40:51'),
+('00001', 'Affenpinscher', 'Pet Dog', 10, '6000', '2017-02-05 07:41:02'),
+('00002', 'Afghan Hound', 'Pet Dog', 10, '4580', '2017-02-05 07:41:02'),
+('00003', 'Akita', 'Pet Dog', 12, '9500', '2017-02-05 07:44:52'),
+('00004', 'America Foxhound', 'Pet Dog', 8, '15000', '2017-02-05 07:44:52'),
+('00005', 'Beagle', 'Pet Dog', 30, '12500', '2017-02-05 07:44:52'),
+('00006', 'Catahoula Leopard Dog', 'Pet Dog', 4, '25000', '2017-02-05 07:44:52'),
+('00007', 'Bengal Cat', 'Pet Cat', 28, '10300', '2017-02-05 07:44:52'),
+('00003', 'Akita', 'Pet Dog', 12, '9500', '2017-02-05 07:45:40'),
+('00004', 'America Foxhound', 'Pet Dog', 8, '15000', '2017-02-05 07:45:40'),
+('00005', 'Beagle', 'Pet Dog', 30, '12500', '2017-02-05 07:45:40'),
+('00006', 'Catahoula Leopard Dog', 'Pet Dog', 4, '25000', '2017-02-05 07:45:40'),
+('00007', 'Bengal Cat', 'Pet Cat', 28, '10300', '2017-02-05 07:45:40'),
+('00008', 'American Curl', 'Pet Cat', 18, '25000', '2017-02-05 07:47:03'),
+('00009', 'British Short Hair', 'Pet Cat', 22, '35000', '2017-02-05 07:47:03'),
+('00007', 'American Curl', 'Pet Cat', 15, '19000', '2017-02-05 07:49:44'),
+('00008', 'Burmese', 'Pet Cat', 27, '29000', '2017-02-05 07:49:44'),
+('00009', 'Cymric', 'Pet Cat', 10, '38000', '2017-02-05 07:49:44'),
+('00010', 'Persian ', 'Pet Cat', 44, '50000', '2017-02-05 07:49:44'),
+('00011', 'Siamese Cat', 'Pet Cat', 16, '27300', '2017-02-05 07:49:44'),
+('00007', 'American Curl', 'Pet Cat', 15, '19000', '2017-02-05 07:49:46'),
+('00008', 'Burmese', 'Pet Cat', 27, '29000', '2017-02-05 07:49:46'),
+('00009', 'Cymric', 'Pet Cat', 10, '38000', '2017-02-05 07:49:46'),
+('00010', 'Persian ', 'Pet Cat', 44, '50000', '2017-02-05 07:49:46'),
+('00011', 'Siamese Cat', 'Pet Cat', 16, '27300', '2017-02-05 07:49:46'),
+('00012', 'Whiskas Cat Food', 'Cat Food', 100, '150', '2017-02-05 07:59:55'),
+('00013', 'Blackwood Chicken Meal and Fie', 'Cat Food', 200, '280', '2017-02-05 07:59:55'),
+('00013', 'Lotus Just Juicy Pork Stew Gra', 'Cat Food', 200, '250', '2017-02-05 07:59:55'),
+('00014', 'Wysong Optimal Vitality Dry Ca', 'Cat Food ', 280, '300', '2017-02-05 07:59:55'),
+('00015', 'Big Bite Bunch of Treats, Beef', 'Dog Food ', 382, '260', '2017-02-05 07:59:55'),
+('00022', 'Red Cap Oranda Goldfish', 'Pet Fish', 110, '25', '2017-02-05 08:03:40'),
+('00023', 'Blue Paradise', 'Pet Fish', 130, '28', '2017-02-05 08:03:40'),
+('00024', 'Syrian Hamster', 'Pet Hamster', 30, '100', '2017-02-05 08:03:40'),
+('00025', 'Dwarf Cambells Russian Hamster', 'Pet Hamster', 27, '150', '2017-02-05 08:03:40'),
+('00026', 'Roborovski Hamster', 'Pet Hamster', 27, '200', '2017-02-05 08:03:40'),
+('00022', 'Red Cap Oranda Goldfish', 'Pet Fish', 110, '25', '2017-02-05 08:03:46'),
+('00023', 'Blue Paradise', 'Pet Fish', 130, '28', '2017-02-05 08:03:46'),
+('00024', 'Syrian Hamster', 'Pet Hamster', 30, '100', '2017-02-05 08:03:46'),
+('00025', 'Dwarf Cambells Russian Hamster', 'Pet Hamster', 27, '150', '2017-02-05 08:03:46'),
+('00026', 'Roborovski Hamster', 'Pet Hamster', 27, '200', '2017-02-05 08:03:46');
 
 -- --------------------------------------------------------
 
@@ -108,13 +102,13 @@ INSERT INTO `user` (`id_user`, `username`, `password`) VALUES
 --
 
 CREATE TABLE `users` (
-  `id` int(150) NOT NULL,
+  `id` int(100) NOT NULL,
   `FirstName` varchar(40) DEFAULT NULL,
   `LastName` varchar(40) DEFAULT NULL,
   `username` varchar(40) NOT NULL,
   `user_email` varchar(40) DEFAULT NULL,
   `user_password` varchar(40) NOT NULL,
-  `acct_type` varchar(10) NOT NULL DEFAULT 'basic' COMMENT '0B,1IM,2POS,3SA'
+  `acct_type` int(10) NOT NULL DEFAULT '0' COMMENT '0B,1IM,2POS,3SA'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -122,13 +116,13 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `FirstName`, `LastName`, `username`, `user_email`, `user_password`, `acct_type`) VALUES
-(40, NULL, NULL, 'user3', NULL, '92877af70a45fd6a2ed7fe81e1236b78', 'SA'),
-(41, NULL, NULL, 'user0', NULL, '3d517fe6ebab7b8cfcf98db6259c8a59', 'Basic'),
-(39, NULL, NULL, 'user2', NULL, '7e58d63b60197ceb55a1c487989a3720', 'Inventory'),
-(38, NULL, NULL, 'user1', NULL, '24c9e15e52afc47c225b757e7bee1f9d', 'POS'),
-(101, 'Super', 'Admin', 'sa', 'sa@gmail.com', 'c12e01f2a13ff5587e1e9e4aedb8242d', 'SA'),
-(51, 'Inventory', 'Admin', 'ia', 'ia@gmail.com', 'b40788f566d1124a95169068077031a9', 'IA'),
-(1, 'POS', 'Admin', 'pa', 'pa@gmail.com', 'e529a9cea4a728eb9c5828b13b22844c', 'PA');
+(40, NULL, NULL, 'user3', NULL, '92877af70a45fd6a2ed7fe81e1236b78', 3),
+(41, NULL, NULL, 'user0', NULL, '3d517fe6ebab7b8cfcf98db6259c8a59', 0),
+(39, NULL, NULL, 'user2', NULL, '7e58d63b60197ceb55a1c487989a3720', 2),
+(38, NULL, NULL, 'user1', NULL, '24c9e15e52afc47c225b757e7bee1f9d', 1),
+(99, 'Super', 'Admin', 'sa', 'sa@gmail.com', 'c12e01f2a13ff5587e1e9e4aedb8242d', 3),
+(51, 'Inventory', 'Admin', 'ia', 'ia@gmail.com', 'b40788f566d1124a95169068077031a9', 2),
+(1, 'POS', 'Admin', 'pa', 'pa@gmail.com', 'e529a9cea4a728eb9c5828b13b22844c', 1);
 
 --
 -- Indexes for dumped tables
@@ -141,12 +135,6 @@ ALTER TABLE `ci_sessions`
   ADD KEY `ci_sessions_timestamp` (`timestamp`);
 
 --
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id_user`);
-
---
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -157,10 +145,10 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT for table `users`
 --
-ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+ALTER TABLE `users`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=201;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
