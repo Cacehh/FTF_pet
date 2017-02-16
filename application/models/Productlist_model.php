7b1948feb282c0e867326b7c35785323c9fd9d32
rolsamps <jerolsampaga@gmail.com>
2017-02-16 20:47:56 +0800

    made changes to productview page

---
+++ application/models/Productlist_model.php
@@ -0,0 +1,20 @@
+<?php
+	class Productlist_model extends CI_Model {
+	
+		    function __construct(){
+				parent::__construct(); // Call the Model constructor
+			}
+			
+			function getProduct() {
+				$query = $this->db->query('SELECT * FROM product_test');
+				
+				if($query->num_rows() > 0) {
+					return $query->result();
+				} else {
+					return NULL;
+				}
+			}
+			
+
+	}
+?>
--- application/views/+pages/productList.php
+++ application/views/+pages/productList.php
@@ -1,259 +1,82 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title><?php echo $title; ?></title>
+	<meta charset="utf-8">
+</head>
 
 <main class="">   
     <div class="row">
         <div class="col s9">
-            <div class="row ">
-                <div class="card col s3 hoverable">
-                    <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" src="<?php echo base_url()?>public/media/products/1.jpg">
-                    </div>
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">
-                            <a href="<?php echo base_url()?>index.php/Pages_ctr/productDetails">Dog food 1</a><i class="material-icons right">more_vert</i></span>
-                        <b><p><a href="#">Add to cart</a><b> | </b> <a href="#">Choices...</a></p></b>
-                    </div>
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">More info...<i class="material-icons right">close</i></span>
-                        <p>This type of dog food is best for puppies aged 1 to 3 months old.</p>
-                    </div>
-                </div>   
-                <div class="card col s3 hoverable">
-                    <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" src="<?php echo base_url()?>public/media/products/1.jpg">
-                    </div>
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Dog food 1<i class="material-icons right">more_vert</i></span>
-                        <b><p><a href="#">Add to cart</a><b> | </b> <a href="#">Choices...</a></p></b>
-                    </div>
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">More info...<i class="material-icons right">close</i></span>
-                        <p>This type of dog food is best for puppies aged 1 to 3 months old.</p>
-                    </div>
-                </div>
-                <div class="card col s3 hoverable">
-                    <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" src="<?php echo base_url()?>public/media/products/1.jpg">
-                    </div>
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Dog food 1<i class="material-icons right">more_vert</i></span>
-                        <b><p><a href="#">Add to cart</a><b> | </b> <a href="#">Choices...</a></p></b>
-                    </div>
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">More info...<i class="material-icons right">close</i></span>
-                        <p>This type of dog food is best for puppies aged 1 to 3 months old.</p>
-                    </div>
-                </div>   
-                <div class="card col s3 hoverable">
-                    <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" src="<?php echo base_url()?>public/media/products/1.jpg">
-                    </div>
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Dog food 1<i class="material-icons right">more_vert</i></span>
-                        <b><p><a href="#">Add to cart</a><b> | </b> <a href="#">Choices...</a></p></b>
-                    </div>
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">More info...<i class="material-icons right">close</i></span>
-                        <p>This type of dog food is best for puppies aged 1 to 3 months old.</p>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="row ">
-                <div class="card col s3 hoverable">
-                    <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" src="<?php echo base_url()?>public/media/products/1.jpg">
-                    </div>
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Dog food 1<i class="material-icons right">more_vert</i></span>
-                        <b><p><a href="#">Add to cart</a><b> | </b> <a href="#">Choices...</a></p></b>
-                    </div>
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">More info...<i class="material-icons right">close</i></span>
-                        <p>This type of dog food is best for puppies aged 1 to 3 months old.</p>
-                    </div>
-                </div>   
-                <div class="card col s3 hoverable">
-                    <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" src="<?php echo base_url()?>public/media/products/1.jpg">
-                    </div>
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Dog food 1<i class="material-icons right">more_vert</i></span>
-                        <b><p><a href="#">Add to cart</a><b> | </b> <a href="#">Choices...</a></p></b>
-                    </div>
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">More info...<i class="material-icons right">close</i></span>
-                        <p>This type of dog food is best for puppies aged 1 to 3 months old.</p>
-                    </div>
-                </div>
-                <div class="card col s3 hoverable">
-                    <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" src="<?php echo base_url()?>public/media/products/1.jpg">
-                    </div>
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Dog food 1<i class="material-icons right">more_vert</i></span>
-                        <b><p><a href="#">Add to cart</a><b> | </b> <a href="#">Choices...</a></p></b>
-                    </div>
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">More info...<i class="material-icons right">close</i></span>
-                        <p>This type of dog food is best for puppies aged 1 to 3 months old.</p>
-                    </div>
-                </div>   
-                <div class="card col s3 hoverable">
-                    <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" src="<?php echo base_url()?>public/media/products/1.jpg">
-                    </div>
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Dog food 1<i class="material-icons right">more_vert</i></span>
-                        <b><p><a href="#">Add to cart</a><b> | </b> <a href="#">Choices...</a></p></b>
-                    </div>
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">More info...<i class="material-icons right">close</i></span>
-                        <p>This type of dog food is best for puppies aged 1 to 3 months old.</p>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="row ">
-                <div class="card col s3 hoverable">
-                    <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" src="<?php echo base_url()?>public/media/products/1.jpg">
-                    </div>
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Dog food 1<i class="material-icons right">more_vert</i></span>
-                        <b><p><a href="#">Add to cart</a><b> | </b> <a href="#">Choices...</a></p></b>
-                    </div>
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">More info...<i class="material-icons right">close</i></span>
-                        <p>This type of dog food is best for puppies aged 1 to 3 months old.</p>
-                    </div>
-                </div>   
-                <div class="card col s3 hoverable">
-                    <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" src="<?php echo base_url()?>public/media/products/1.jpg">
-                    </div>
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Dog food 1<i class="material-icons right">more_vert</i></span>
-                        <b><p><a href="#">Add to cart</a><b> | </b> <a href="#">Choices...</a></p></b>
-                    </div>
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">More info...<i class="material-icons right">close</i></span>
-                        <p>This type of dog food is best for puppies aged 1 to 3 months old.</p>
-                    </div>
-                </div>
-                <div class="card col s3 hoverable">
-                    <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" src="<?php echo base_url()?>public/media/products/1.jpg">
-                    </div>
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Dog food 1<i class="material-icons right">more_vert</i></span>
-                        <b><p><a href="#">Add to cart</a><b> | </b> <a href="#">Choices...</a></p></b>
-                    </div>
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">More info...<i class="material-icons right">close</i></span>
-                        <p>This type of dog food is best for puppies aged 1 to 3 months old.</p>
-                    </div>
-                </div>   
-                <div class="card col s3 hoverable">
-                    <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" src="<?php echo base_url()?>public/media/products/1.jpg">
-                    </div>
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Dog food 1<i class="material-icons right">more_vert</i></span>
-                        <b><p><a href="#">Add to cart</a><b> | </b> <a href="#">Choices...</a></p></b>
-                    </div>
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">More info...<i class="material-icons right">close</i></span>
-                        <p>This type of dog food is best for puppies aged 1 to 3 months old.</p>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="row ">
-                <div class="card col s3 hoverable">
-                    <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" src="<?php echo base_url()?>public/media/products/1.jpg">
-                    </div>
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Dog food 1<i class="material-icons right">more_vert</i></span>
-                        <b><p><a href="#">Add to cart</a><b> | </b> <a href="#">Choices...</a></p></b>
-                    </div>
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">More info...<i class="material-icons right">close</i></span>
-                        <p>This type of dog food is best for puppies aged 1 to 3 months old.</p>
-                    </div>
-                </div>   
-                <div class="card col s3 hoverable">
-                    <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" src="<?php echo base_url()?>public/media/products/1.jpg">
-                    </div>
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Dog food 1<i class="material-icons right">more_vert</i></span>
-                        <b><p><a href="#">Add to cart</a><b> | </b> <a href="#">Choices...</a></p></b>
-                    </div>
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">More info...<i class="material-icons right">close</i></span>
-                        <p>This type of dog food is best for puppies aged 1 to 3 months old.</p>
-                    </div>
-                </div>
-                <div class="card col s3 hoverable">
-                    <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" src="<?php echo base_url()?>public/media/products/1.jpg">
-                    </div>
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Dog food 1<i class="material-icons right">more_vert</i></span>
-                        <b><p><a href="#">Add to cart</a><b> | </b> <a href="#">Choices...</a></p></b>
-                    </div>
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">More info...<i class="material-icons right">close</i></span>
-                        <p>This type of dog food is best for puppies aged 1 to 3 months old.</p>
-                    </div>
-                </div>   
-                <div class="card col s3 hoverable">
-                    <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" src="<?php echo base_url()?>public/media/products/1.jpg">
-                    </div>
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Dog food 1<i class="material-icons right">more_vert</i></span>
-                        <b><p><a href="#">Add to cart</a><b> | </b> <a href="#">Choices...</a></p></b>
-                    </div>
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">More info...<i class="material-icons right">close</i></span>
-                        <p>This type of dog food is best for puppies aged 1 to 3 months old.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col s3">
-            <div class="blue-grey lighten-5 z-depth-1">
-                <br>
-                <h3 class="center">WISHLIST</h3>
-                <table>
-                    <thead>
-                        <tr>
-                            <th data-field="name">Item Name</th>
-                            <th data-field="price">Item Price</th>
-                            <th data-field="edit">Edit</th>
-                        </tr>
-                    </thead>
-                
-                    <tbody>
-                        <tr>
-                            <td>Pedigree Chewies</td>
-                            <td>&#8369; 399.00</td>
-                            <td>
-                            <a href=""><i class="material-icons">mode_edit</i></a>
-                            </td>         
-                        </tr>
-                        <tr>
-                            <td>Colar - Red</td>
-                            <td>&#8369; 43.00</td>
-                            <td>
-                            <a href=""><i class="material-icons">mode_edit</i></a>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
+			<?php
+				
+				$count = 1;
+				
+				foreach ($product as $row) {
+							
+					if ($count%4 == 1){  
+							 echo "<div class=\"row\">";
+						}
+						
+					echo "<div class=\"card col s3 hoverable\">";
+					echo "<div class=\"card-image waves-effect waves-block waves-light\">";
+					echo "<img class=\"activator\" src=".base_url()."public/media/products/1.jpg>";
+					echo "</div>";
+					echo "<div class=\"card-content\">";
+					echo "<span class=\"card-title activator grey-text text-darken-4\">";
+					echo "<a href=\"" .base_url(). "index.php/Pages_ctr/productDetails\">" .$row ->name. "</a><i class=\"material-icons right\">more_vert</i></span>";
+					echo "<b><p><a href=\"#\">Add to cart</a><b> | </b> <a href=\"#\">Choices...</a></p></b>";
+					echo "</div>";
+					echo "<div class=\"card-reveal\">";
+					echo "<span class=\"card-title grey-text text-darken-4\">More info...<i class=\"material-icons right\">close</i></span>";
+					echo "<p>" .$row ->price. " dollars</p>";
+					echo "</div>";
+					echo "</div>";
+				
+					if ($count%4 == 0){
+							 echo "</div>";
+					}
+						
+					$count++;
+				}			
+			?>
+		</div>
+	
+				<div class="col s3">
+					<div class="blue-grey lighten-5 z-depth-1">
+						<br>
+						<h3 class="center">WISHLIST</h3>
+						<table>
+							<thead>
+								<tr>
+									<th data-field="name">Item Name</th>
+									<th data-field="price">Item Price</th>
+									<th data-field="edit">Edit</th>
+								</tr>
+							</thead>
+						
+							<tbody>
+								<tr>
+									<td>Pedigree Chewies</td>
+									<td>&#8369; 399.00</td>
+									<td>
+									<a href=""><i class="material-icons">mode_edit</i></a>
+									</td>         
+								</tr>
+								<tr>
+									<td>Colar - Red</td>
+									<td>&#8369; 43.00</td>
+									<td>
+									<a href=""><i class="material-icons">mode_edit</i></a>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
 </main>
