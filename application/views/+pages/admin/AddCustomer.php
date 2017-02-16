127dc9536307356dc4dbb54330ef47c5e8772a78
Megan Avila <megandrea.avila@gmail.com>
2017-02-16 20:00:48 +0800

    Add Customer already working.

--- application/views/+pages/admin/AddCustomer.php
+++ application/views/+pages/admin/AddCustomer.php
@@ -15,15 +15,18 @@
     ?>
 
     <div class="row">
+
+    <h4>Add Customer Account</h4>
+
       <div class="input-field col s12">
 
           <!-- First Name -->
-          <div class="input-field col s5">
+          <div class="input-field col s4">
               <i class="material-icons prefix">account_circle</i>
                    <?php
                         $fname = array(
-                            'id'        => 'fname','name' => 'fname','maxlength' => '20',
-                            'pattern'   => '.{2,20}','length' => '20','class' => 'validate',
+                            'id'        => 'fname','name' => 'fname','maxlength' => '25',
+                            'pattern'   => '.{2,25}','length' => '25','class' => 'validate',
                         );
                         echo form_input($fname);
                     ?>  
@@ -31,12 +34,12 @@
                     <?php echo form_error('fname'); ?>
            </div>
 
-        <!-- Last Name -->
-         <div class="input-field col s5">
+         <!-- Last Name -->
+         <div class="input-field col s4">
                     <?php
                         $lname = array(
-                            'id'        => 'lname','name' => 'lname','maxlength' => '20',
-                            'pattern'   => '.{2,20}','length' => '20','class' => 'validate',
+                            'id'        => 'lname','name' => 'lname','maxlength' => '25',
+                            'pattern'   => '.{2,25}','length' => '25','class' => 'validate',
                         );
                         echo form_input($lname);
                     ?>  
@@ -44,7 +47,105 @@
                     <?php echo form_error('lname'); ?>
         </div>
 
+         <!-- Middle Initial -->
+         <div class="input-field col s4">
+                    <?php
+                        $mname = array(
+                            'id'        => 'mname','name' => 'mname','maxlength' => '2',
+                            'pattern'   => '.{1,2}','length' => '2','class' => 'validate',
+                        );
+                        echo form_input($mname);
+                    ?>  
+                    <label for="lname" data-error="Must be atleast 2 characters" >M.I</label>
+                    <?php echo form_error('mname'); ?>
+        </div>
+
       </div>
+
+       <div class="input-field col s12">
+
+          <!-- Gender Type -->
+          <div class="input-field col s3">
+              <i class="material-icons prefix">person_pin</i>
+              <select id="gender" name="gender">
+                <option value="Choose" disabled selected>Choose</option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+              </select>
+              <label>Gender</label>
+          </div>
+
+          <div class="input-field col s5">
+              <i class="material-icons prefix">email</i>
+                   <?php
+                        $demail = array(
+                            'id'        => 'demail','name' => 'demail','maxlength' => '30',
+                            'pattern'   => '.{2,30}','length' => '30','class' => 'validate',
+                        );
+                        echo form_input($demail);
+                    ?>  
+                    <label for="fname" data-error="Must be atleast 2 characters" >E-mail</label>
+                    <?php echo form_error('demail'); ?>
+           </div>
+
+           <!-- Mobile number -->
+          <div class="input-field col s4">
+              <i class="material-icons prefix">local_phone</i>
+                  <?php
+                    $dmobile = array(
+                      'id'        => 'dmobile','name' => 'dmobile','maxlength' => '11',
+                      'pattern'   => '.{11,11}','length' => '11','class' => 'validate',
+                      );
+                    echo form_input($dmobile);
+                  ?>  
+                  <label for="dmobile" data-error="Must be a valid number" >Number</label>
+                  <?php echo form_error('dmobile'); ?>
+          </div>
+       </div>
+
+        <div class="input-field col s12">
+             <div class="input-field col s6">
+                  <i class="material-icons prefix">my_location</i>
+                    <?php
+                        $street = array(
+                            'id'        => 'street','name' => 'street','maxlength' => '25',
+                            'pattern'   => '.{2,25}','length' => '25','class' => 'validate',
+                        );
+                        echo form_input($street);
+                    ?>  
+                    <label for="street" data-error="Must be atleast 2 characters" >Street</label>
+                    <?php echo form_error('street'); ?>
+              </div>
+
+              <div class="input-field col s6">
+                  <i class="material-icons prefix">business</i>
+                    <?php
+                        $city = array(
+                            'id'        => 'city','name' => 'city','maxlength' => '25',
+                            'pattern'   => '.{2,25}','length' => '25','class' => 'validate',
+                        );
+                        echo form_input($city);
+                    ?>  
+                    <label for="city" data-error="Must be atleast 2 characters" >City</label>
+                    <?php echo form_error('city'); ?>
+              </div>
+        </div>
+
+        <div class="input-field col s12">
+            <div class="input-field col s12">
+                  <i class="material-icons prefix">room</i>
+                    <?php
+                        $barsub = array(
+                            'id'        => 'barsub','name' => 'barsub','maxlength' => '25',
+                            'pattern'   => '.{2,25}','length' => '25','class' => 'validate',
+                        );
+                        echo form_input($barsub);
+                    ?>  
+                    <label for="street" data-error="Must be atleast 2 characters" >Barangay/Subdivision</label>
+                    <?php echo form_error('barsub'); ?>
+              </div>
+        </div>
+
     </div>
 
     <?php 
