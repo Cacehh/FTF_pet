7c736e9d3ab79f654ab13991afd25e4e0fe6f179
Megan Avila <megandrea.avila@gmail.com>
2017-02-18 13:19:28 +0800

    edited the link forthe add Products and add Pets button

--- application/views/+pages/admin/a_header.php
+++ application/views/+pages/admin/a_header.php
@@ -130,10 +130,10 @@
 						<li class="bold center deep-orange accent-2">
 							<a class="white-text collapsible-header">Quick Actions</a>
 						</li>
-						<li class=" bold"><a href="<?php echo base_url()?>index.php/Admin_Product_ctr/addProducts" class="white-text waves-effect waves-orange btn deep-orange accent-2">
+						<li class=" bold"><a href="<?php echo base_url()?>index.php/Inventory_ctr/addProducts" class="white-text waves-effect waves-orange btn deep-orange accent-2">
 							<i class="tiny material-icons left">library_add</i>PRODUCT
 						</a></li>	
-						<li class=" bold"><a href="<?php echo base_url()?>index.php/Admin_Product_ctr/addPets" class="white-text waves-effect waves-orange btn deep-orange accent-2">
+						<li class=" bold"><a href="<?php echo base_url()?>index.php/inventory_ctr/addPets" class="white-text waves-effect waves-orange btn deep-orange accent-2">
 							<i class="tiny material-icons left">library_add</i>PET
 						</a></li>	
 						<li class=" bold"><a href="#" class="white-text waves-effect waves-orange btn deep-orange accent-2">
