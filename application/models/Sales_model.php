8c45ea9702df8c10c160bef4b3b73d552e3dad1a
JASON-PC\Jason <obenajason@gmail.com>
2017-02-18 14:41:51 +0800

    used the porivide salesledger page to be displayed in the Sales Ledger menu

--- application/models/Sales_model.php
+++ application/models/Sales_model.php
@@ -1,9 +1,13 @@
 <?php if ( !defined ('BASEPATH')) exit('No direct script access allowed');
 
-class petinv_model extends CI_Model {
+class Sales_model extends CI_Model {
 
-    function __construct()
-    {
+    function __construct() {
         parent::__construct();
-    }
+
+    function form_get($data){
+		// test only function
+		$this->db->insert('invoice', $data);
+	}
+}
 ?>
---
+++ application/views/+pages/admin/salesledger.php
@@ -0,0 +1,72 @@
+<br>
+<main>
+		
+<!--Add pets tab-->
+	<div class="row">
+		<div class="row">
+			<h4 class="offset-s1 col s11">Sales Ledger</h4>
+		</div>
+		<div class="row grey lighten-1">
+			<div class="offset-s1 col s11">
+				<p class="flow-text">Edit and void transactions, or send them to Xero. To process a return or continue sales, go to Sales History.</p>
+			</div>
+		</div>
+        <div class="row container">
+            <div class="col s12">
+			<table>
+			<thead>
+			<tr>
+			<th data-field="id">Date</th>
+			<th data-field="name">User</th>
+			<th data-field="price">Register</th>
+			<th data-field="price">Receipt</th>
+			<th data-field="price">Customer</th>
+			<th data-field="price">Note</th>
+			<th data-field="price">Status</th>
+			</tr>
+			</thead>
+
+			<tbody>
+			<tr>
+			<td>09-02-16</td>
+			<td>Jhon</td>
+			<td>Done</td>
+			<td>#2012993</td>
+			<td>Kim Kar</td>
+			<td>N/A</td>
+			<td>Fully paid</td>
+			</tr>
+			<tr>
+			<td>09-02-16</td>
+			<td>Jhon</td>
+			<td>Done</td>
+			<td>#2012993</td>
+			<td>Kim Kar</td>
+			<td>N/A</td>
+			<td>Fully paid</td>
+			</tr>
+			<tr>
+			<td>09-02-16</td>
+			<td>Jhon</td>
+			<td>Done</td>
+			<td>#2012993</td>
+			<td>Kim Kar</td>
+			<td>N/A</td>
+			<td>Fully paid</td>
+			</tr>
+			<tr>
+			<td>09-02-16</td>
+			<td>Jhon</td>
+			<td>Done</td>
+			<td>#2012993</td>
+			<td>Kim Kar</td>
+			<td>N/A</td>
+			<td>Fully paid</td>
+			</tr>
+
+			</tbody>
+			</table>
+            </div>
+        </div>
+	</div>
+</main>
