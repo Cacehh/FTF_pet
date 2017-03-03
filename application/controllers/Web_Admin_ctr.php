1bcf421109d91c7a732b0c61fca738e41fae4fee
Rolf Cayce Dy <caycedy@gmail.com>
2017-03-03 12:21:50 +0800

    Web Admin Fix

--- application/controllers/Web_Admin_ctr.php
+++ application/controllers/Web_Admin_ctr.php
@@ -2,157 +2,133 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Web_Admin_ctr extends CI_Controller {
-    
+
     public function index()
     {
-<<<<<<< HEAD
         $data['title'] = 'FTNF | Login';
         $this->load->view('snips/a_start', $data);
-=======
         $data['title'] = 'FTNF | Web Admin';
         $this->load->view('snips/a_start' , $data);
->>>>>>> c5b630fc878c93e5d7f0a5d350cba6d9fb2e93c1
         $this->load->view('snips/css_materialize');
         $this->load->view('snips/css_materialize_icon');
-        
+
         $this->load->view('+pages/web_admin/a_login');
-		
+
 		$this->load->view('snips/js_jquery300');
         $this->load->view('snips/js_materialize');
 		$this->load->view('snips/z_end');
-    }   
-	
+    }
+
 	public function register()
     {
         $data['title'] = 'FTNF | Register';
-<<<<<<< HEAD
         $this->load->view('snips/a_start', $data);
-=======
         $this->load->view('snips/a_start' , $data);
->>>>>>> c5b630fc878c93e5d7f0a5d350cba6d9fb2e93c1
         $this->load->view('snips/css_materialize');
         $this->load->view('snips/css_materialize_icon');
-        
+
         $this->load->view('+pages/web_admin/a_register');
-		
+
 		$this->load->view('snips/js_jquery300');
         $this->load->view('snips/js_materialize');
 		$this->load->view('snips/z_end');
-    } 
+    }
 	public function forgotpassword()
     {
-<<<<<<< HEAD
         $data['title'] = 'FTNF | Password';
         $this->load->view('snips/a_start', $data);
-=======
         $data['title'] = 'FTNF | Web Admin';
         $this->load->view('snips/a_start' , $data);
->>>>>>> c5b630fc878c93e5d7f0a5d350cba6d9fb2e93c1
         $this->load->view('snips/css_materialize');
         $this->load->view('snips/css_materialize_icon');
-        
+
         $this->load->view('+pages/web_admin/a_forgotpassword');
-		
+
         $this->load->view('snips/js_jquery300');
         $this->load->view('snips/js_materialize');
 		$this->load->view('snips/z_end');
     }
-	
+
 	public function dashboard()
     {
-<<<<<<< HEAD
         $data['title'] = 'FTNF | Dashboard';
         $this->load->view('snips/a_start', $data);
-=======
         $data['title'] = 'FTNF | Web Admin';
         $this->load->view('snips/a_start' , $data);
->>>>>>> c5b630fc878c93e5d7f0a5d350cba6d9fb2e93c1
         $this->load->view('snips/css_materialize');
         $this->load->view('snips/css_materialize_icon');
 		$this->load->view('+pages/web_admin/a_header');
-		
+
         $this->load->view('+pages/web_admin/dashboard');
-		
+
 		$this->load->view('snips/js_jquery300');
         $this->load->view('snips/js_materialize');
 		$this->load->view('snips/z_end');
-    }	
+    }
 
     public function products()
     {
         $data['title'] = 'FTNF | Products';
-<<<<<<< HEAD
         $this->load->view('snips/a_start', $data);
-=======
         $this->load->view('snips/a_start' , $data);
->>>>>>> c5b630fc878c93e5d7f0a5d350cba6d9fb2e93c1
         $this->load->view('snips/css_materialize');
         $this->load->view('snips/css_materialize_icon');
         $this->load->view('+pages/web_admin/a_header');
-        
+
         $this->load->view('+pages/web_admin/product_items');
-        
+
         $this->load->view('snips/js_jquery300');
         $this->load->view('snips/js_materialize');
         $this->load->view('snips/z_end');
-    }  
+    }
 
     public function addProducts()
     {
-<<<<<<< HEAD
         $data['title'] = 'FTNF | Add Products';
         $this->load->view('snips/a_start', $data);
-=======
         $data['title'] = 'FTNF | Products';
         $this->load->view('snips/a_start' , $data);
->>>>>>> c5b630fc878c93e5d7f0a5d350cba6d9fb2e93c1
         $this->load->view('snips/css_materialize');
         $this->load->view('snips/css_materialize_icon');
         $this->load->view('+pages/web_admin/a_header');
-        
+
         $this->load->view('+pages/web_admin/add_products2');
-        
+
         $this->load->view('snips/js_jquery300');
         $this->load->view('snips/js_materialize');
         $this->load->view('snips/z_end');
-    }  
-    
+    }
+
     public function pets()
     {
         $data['title'] = 'FTNF | Pets';
-<<<<<<< HEAD
         $this->load->view('snips/a_start', $data);
-=======
         $this->load->view('snips/a_start' , $data);
->>>>>>> c5b630fc878c93e5d7f0a5d350cba6d9fb2e93c1
         $this->load->view('snips/css_materialize');
         $this->load->view('snips/css_materialize_icon');
         $this->load->view('+pages/web_admin/a_header');
-        
+
         $this->load->view('+pages/web_admin/product_pets');
-        
+
         $this->load->view('snips/js_jquery300');
         $this->load->view('snips/js_materialize');
         $this->load->view('snips/z_end');
-    }   
-    
+    }
+
     public function addPets() {
-<<<<<<< HEAD
         $data['title'] = 'FTNF | Add Pets';
         $this->load->view('snips/a_start', $data);
-=======
         $data['title'] = 'FTNF | Pets';
         $this->load->view('snips/a_start' , $data);
->>>>>>> c5b630fc878c93e5d7f0a5d350cba6d9fb2e93c1
         $this->load->view('snips/css_materialize');
         $this->load->view('snips/css_materialize_icon');
         $this->load->view('+pages/web_admin/a_header');
-        
+
         $this->load->view('+pages/web_admin/add_pets2');
-        
+
         $this->load->view('snips/js_jquery300');
         $this->load->view('snips/js_materialize');
         $this->load->view('snips/z_end');
-    }	
-	
+    }
+
 }
